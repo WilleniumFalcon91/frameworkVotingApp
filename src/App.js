951ed4ba1f89fe401eebde635754b7c0e023ac74
William Elliott <wcelliott1991@gmail.com>
2017-12-01 13:55:50 -0500

    styles basic header layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { VOTE_ANGULAR, VOTE_REACT, VOTE_VUE } from './actions';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.store = this.props.store;
+  }
+
+  handleVoteAngular = () => {
+    this.store.dispatch(VOTE_ANGULAR());
+  }
+  handleVoteReact = () => {
+    this.store.dispatch(VOTE_REACT());
+  }
+  handleVoteVue = () => {
+    this.store.dispatch(VOTE_VUE());
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="jumbotron" style={{'textAlign': 'center'}}>
+          <img className="angular-logo" src="../angular.png"/>
+          <img className="react-logo" src="../React-icon.svg"/>
+          <img className="vue-logo" src="../Vue.png"/>
+        </div>
       </div>
     );
   }
